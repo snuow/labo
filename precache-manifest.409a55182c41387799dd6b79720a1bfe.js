ace19edcc107e2099856aae1b865246f017166a1
snuow <24.nishi.yuuki@gmail.com>
2020-05-19 22:27:10 +0900

    Updates

---
+++ precache-manifest.409a55182c41387799dd6b79720a1bfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5bf347075938eee042291447d5db24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99cd29bd6b6ae2222fc6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b79cc30d314bb605b1b4",
+    "url": "/static/js/2.f1e5846f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f1e5846f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99cd29bd6b6ae2222fc6",
+    "url": "/static/js/main.32b10235.chunk.js"
+  },
+  {
+    "revision": "eaafe1b27807f244ea50",
+    "url": "/static/js/runtime-main.d9ef5893.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/snuow.github.io/precache-manifest.2e66cedfca8f0faf92c7ca80a54ab03a.js"
+  "/precache-manifest.409a55182c41387799dd6b79720a1bfe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/snuow.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
